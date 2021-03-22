0c144c820c23b7754f4888a15a8fffff79c151e7
frankytarun <tarungupta0208@gmail.com>
2021-03-23 00:24:33 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-vjf65d
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-vjf65d)
---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [ ],
+  imports: [
+    
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
